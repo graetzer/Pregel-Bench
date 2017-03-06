1493e2f0bc7da36856b4ad67d209397299a9a55d
Simon Grätzer <simon@graetzer.org>
2017-03-06 11:12:04 +0100

    Fixed distance calculation

--- giraph/shortestpath/ShortestPath.java
+++ giraph/shortestpath/ShortestPath.java
@@ -46,7 +46,7 @@ public class ShortestPath extends BasicComputation<
     if (minDist < vertex.getValue().get()) {
       vertex.setValue(new DoubleWritable(minDist));
       for (Edge<LongWritable, NullWritable> edge : vertex.getEdges()) {
-        double distance = minDist + edge.getValue().get();
+        double distance = minDist + 1;//edge.getValue().get();
         sendMessage(edge.getTargetVertexId(), new DoubleWritable(distance));
       }
     }
